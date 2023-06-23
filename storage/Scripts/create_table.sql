f5c600558c6eb7340bae9fbde3d9cb263792c7de
paul solarin <paulsolarin@gmail.com>
2023-06-23 16:26:22 -0400

    create staging table

--- storage/Scripts/create_table.sql
+++ storage/Scripts/create_table.sql
@@ -1,4 +1,39 @@
-SELECT *
-from TEST ;
+drop table job_desc_analysis;
 
-DESCRIBE michaelwschmidt_job_description_analysis;
+Create Table job_desc_analysis (
+id SERIAL  primary key,
+locations varchar (50) not null,
+site varchar (50),
+posting_date datetime not null,
+url varchar (255),
+title varchar (100),
+job_description text not null,
+company_name varchar (100),
+salary int,
+salary_min int,
+salary_types int,
+salary_currency_code int,
+salary_max int
+
+)
+
+Desc job_desc_analysis;
+
+Create Table temp_job_desc_analysis (
+id SERIAL  primary key,
+locations varchar (50) not null,
+site varchar (50),
+posting_date datetime not null,
+url varchar (255),
+title varchar (100),
+job_description text not null,
+company_name varchar (100),
+salary int,
+salary_min int,
+salary_types int,
+salary_currency_code int,
+salary_max int
+
+);
+
+desc temp_job_desc_analysis;
